775a1315ed780c1046401f47680f0e7fc7e21085
Moe Ko Ko Oo <moekokooo1994@gmail.com>
2025-05-19 23:48:20 +0630

    Initial Commit

---
+++ something/somethingApp.swift
@@ -0,0 +1,17 @@
+//
+//  somethingApp.swift
+//  something
+//
+//  Created by Moe Ko Ko Oo on 19/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct somethingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
